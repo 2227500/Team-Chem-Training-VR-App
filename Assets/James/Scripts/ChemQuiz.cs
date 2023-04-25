7507c51cedfab601747a4128c0205c627c614bb4
Mead955568 <955568@swansea.ac.uk>
2023-04-25 12:43:22 +0100

    Basic Quiz setup

--- Assets/James/Scripts/ChemQuiz.cs
+++ Assets/James/Scripts/ChemQuiz.cs
@@ -4,7 +4,9 @@ using TMPro;
 public class ChemQuiz : MonoBehaviour
 {
     public TMP_Text questionText;
+
     public TMP_Text feedbackText;
+
 
     public Button Button1;
     public Button Button2;
@@ -24,11 +26,11 @@ public class ChemQuiz : MonoBehaviour
         {
             feedbackText.text = "Correct!";
         }
-        else if (currentQuestion == 3 && answer == 4)
+        else if (currentQuestion == 3 && answer == 3)
         {
             feedbackText.text = "Correct!";
         }
-        else if (currentQuestion == 4 && answer == 2)
+        else if (currentQuestion == 4 && answer == 1)
         {
             feedbackText.text = "Correct!";
         }
@@ -53,43 +55,43 @@ public class ChemQuiz : MonoBehaviour
             // Update the question text and answer options for the next question
             if (currentQuestion == 2)
             {
-                questionText.text = "Question " + currentQuestion + ": What is the capital of France?";
-                Button1.GetComponentInChildren<TMP_Text>().text = "Paris";
-                Button2.GetComponentInChildren<TMP_Text>().text = "London";
-                Button3.GetComponentInChildren<TMP_Text>().text = "Berlin";
-                Button4.GetComponentInChildren<TMP_Text>().text = "Madrid";
+                questionText.text = "Question " + currentQuestion + ": Which of these components is the condenser?";
+                Button1.GetComponentInChildren<TMP_Text>().text = "";
+                Button2.GetComponentInChildren<TMP_Text>().text = "";
+                Button3.GetComponentInChildren<TMP_Text>().text = "";
+                Button4.GetComponentInChildren<TMP_Text>().text = "";
             }
             else if (currentQuestion == 3)
             {
-                questionText.text = "Question " + currentQuestion + ": Who wrote The Great Gatsby?";
-                Button1.GetComponentInChildren<TMP_Text>().text = "Ernest Hemingway";
-                Button2.GetComponentInChildren<TMP_Text>().text = "F. Scott Fitzgerald";
-                Button3.GetComponentInChildren<TMP_Text>().text = "William Faulkner";
-                Button4.GetComponentInChildren<TMP_Text>().text = "John Steinbeck";
+                questionText.text = "Question " + currentQuestion + ": At the start of the experiment which valve do you not close?";
+                Button1.GetComponentInChildren<TMP_Text>().text = "V12";
+                Button2.GetComponentInChildren<TMP_Text>().text = "V5";
+                Button3.GetComponentInChildren<TMP_Text>().text = "V4";
+                Button4.GetComponentInChildren<TMP_Text>().text = "V8";
             }
             else if (currentQuestion == 4)
             {
-                questionText.text = "Question " + currentQuestion + ": Which planet is closest to the sun?";
-                Button1.GetComponentInChildren<TMP_Text>().text = "Venus";
-                Button2.GetComponentInChildren<TMP_Text>().text = "Mars";
-                Button3.GetComponentInChildren<TMP_Text>().text = "Mercury";
-                Button4.GetComponentInChildren<TMP_Text>().text = "Jupiter";
+                questionText.text = "Question " + currentQuestion + ": When you started P1 what did you set the feed flowrate to?";
+                Button1.GetComponentInChildren<TMP_Text>().text = "60%";
+                Button2.GetComponentInChildren<TMP_Text>().text = "75%";
+                Button3.GetComponentInChildren<TMP_Text>().text = "50%";
+                Button4.GetComponentInChildren<TMP_Text>().text = "40%";
             }
             else if (currentQuestion == 5)
             {
-                questionText.text = "Question " + currentQuestion + ": Who painted the Mona Lisa?";
-                Button1.GetComponentInChildren<TMP_Text>().text = "Vincent van Gogh";
-                Button2.GetComponentInChildren<TMP_Text>().text = "Pablo Picasso";
-                Button3.GetComponentInChildren<TMP_Text>().text = "Leonardo da Vinci";
-                Button4.GetComponentInChildren<TMP_Text>().text = "Claude Monet";
+                questionText.text = "Question " + currentQuestion + ": ?";
+                Button1.GetComponentInChildren<TMP_Text>().text = "";
+                Button2.GetComponentInChildren<TMP_Text>().text = "";
+                Button3.GetComponentInChildren<TMP_Text>().text = "";
+                Button4.GetComponentInChildren<TMP_Text>().text = "";
             }
             else
             {
-                questionText.text = "Question " + currentQuestion + ": What is the largest mammal on Earth?";
-                Button1.GetComponentInChildren<TMP_Text>().text = "Elephant";
-                Button2.GetComponentInChildren<TMP_Text>().text = "Whale";
-                Button3.GetComponentInChildren<TMP_Text>().text = "Giraffe";
-                Button4.GetComponentInChildren<TMP_Text>().text = "Hippopotamus";
+                questionText.text = "Question " + currentQuestion + ": Which piece of safety equipment did we not use?";
+                Button1.GetComponentInChildren<TMP_Text>().text = "";
+                Button2.GetComponentInChildren<TMP_Text>().text = "";
+                Button3.GetComponentInChildren<TMP_Text>().text = "";
+                Button4.GetComponentInChildren<TMP_Text>().text = "";
             }
         }
     }
