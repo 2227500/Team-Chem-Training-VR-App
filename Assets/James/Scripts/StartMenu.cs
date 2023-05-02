5b98c10806b788f40d3a98a5719fdfb43703d4ac
Mead955568 <955568@swansea.ac.uk>
2023-05-02 17:42:45 +0100

    Added Basci Menu

---
+++ Assets/James/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(LoadStart);
+    }
+
+    public void LoadStart()
+    {
+        SceneManager.LoadScene("OnBoardingScene_Exp");
+    }
+}
